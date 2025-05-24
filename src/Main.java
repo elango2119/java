4101e8660f51f8f6234e869eda97092493097f53
elango2119 <elangokannan24@gmail.com>
2025-05-24 17:41:19 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main {
+    public static void main(String[] args) {
+        int a = 21;
+        int b = 0;
+        int c = 0;
+        try {
+
+            c = a / b;
+        } catch (ArithmeticException e) {
+            System.out.println("Error: Divide by 0 not allowed");
+
+        }
+    }
+}
+
